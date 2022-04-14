c9238999959c192a8ac75d14d8b979c8a1ee47d2
Mylena <102628246+MylenaAndrade@users.noreply.github.com>
2022-04-14 19:00:28 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,27 @@
 #include<stdio.h>
-#include<stdlib.h>
+/*           O IMC (√çndice de Massa Corporal)
+        √© um crit√©rio da Organiza√ß√£o Mundial de Sa√∫de
+        para dar uma indica√ß√£o sobre a condi√ß√£o de
+        peso de uma pessoa adulta. A f√≥rmula √©:
+
+              IMC = peso / altura2
+
+  Elabore um algoritmo que, dados o peso e a altura de um adulto, determine 
+        a sua condi√ß√£o de acordo com a tabela abaixo:
+          --------------------------------------------  
+              IMC em adultos   |     Condi√ß√£o
+          --------------------------------------------
+                IMC < 18,5     |   Abaixo do peso
+            18,5 ‚â§ IMC < 25,0  |     Peso ideal
+            25,0 ‚â§ IMC < 30,0  |     Sobrepeso
+            30,0 ‚â§ IMC < 35,0  |   Obesidade grau I
+            35,0 ‚â§ IMC < 40,0  |   Obesidade grau II
+              IMC ‚â• 40,0       |   Obesidade grau III
+          ---------------------------------------------
+*/
   void main(){
-/* PRIMEIRO EXERCICIO
-  //declaraÁ„o de vari·veis
+
+  //declara√ß√£o de vari√°veis
     float peso, altura;
     float IMC;
   //inicio
@@ -40,214 +59,7 @@
                             printf("OBESIDADE GRAU III");
                             }
 
-*/
-
-/* SEGUNDO EXERCICIO
- //declaraÁ„o de vari·veis
-  float peso, pesoP;
-  int N;
-
- //inicio
-    printf("Digite o seu peso:\n ");
-    scanf("%f",&peso);
-    printf("Escolha 1 numero:\n ");
-    scanf("%d",&N);
-     if(N==1)
-     {
-        pesoP=(peso/10)*0.37;
-     }
-     else
-        if(N==2)
-        {
-           pesoP=(peso/10)*0.88;
-        }
-        else
-            if(N==3)
-            {
-               pesoP=(peso/10)*0.38;
-            }
-            else
-                if(N==4)
-                {
-                   pesoP=(peso/10)*2.64;
-                }
-                else
-                    if(N==5)
-                    {
-                       pesoP=(peso/10)*1.15;
-                    }
-                    else
-                        {
-                            pesoP=(peso/10)*1.17;
-                        }
-        printf("Seu peso no planeta escolhido e:%f ",pesoP);
-*/
-  /*TERCEIRO EXERCICIO
-   //declaraÁ„o de vari·veis
-        float produto, valor, sJuros, cJuros;
-  //inicio
-        printf("Informe o preco do seu produto: \n R$");
-        scanf("%f",&produto);
-            valor=produto-(produto*0.1);
-        printf("O preco do produto com 10 por cento de desconto e de %f reais\n",valor);
-            sJuros=(produto/5);
-        printf("O valor da prestacao, sem juros, em 5x e de %f reais\n",sJuros);
-            cJuros=(10*(produto*0,2));
-        printf("O valor da prestacao com parcelamento de 10x, com juros de 20 por cento, e de %f reais",cJuros);
-  */
-
-  /*QUARTO EXERCICIO
-   //declaraÁ„o de vari·veis
-    int km, kmL, CC;
-    float preco, total;
-  //inicio
-        printf("Informe o percurso da viagem em Km: ");
-        scanf("%d",&km);
-        printf("Informe a quantidade de Km que o carro percorre com 1L de combustivel: ");
-        scanf("%d",&kmL);
-        printf("Informe o preco do litro do combustivel: ");
-        scanf("%f",&preco);
-            CC=km/kmL;
-            printf("A quantidade de combustivel, em litros, consumida durante a viagem e de: %dL \n",CC);
-            total=CC*preco;
-            printf("O custo total de combustivel e de: R$ %f",total);
-
-*/
-  /*QUINTO EXERCICIO
-    //declaraÁ„o de vari·veis
-        int codigo, quant;
-        float valor;
-
-    //inicio
-        printf("Insira o codigo do pedido: ");
-        scanf("%d",&codigo);
-        printf("Insira a quantidade de itens: ");
-        scanf("%d",&quant);
-            if(codigo==100)
-                {
-                valor=(quant*3.50);
-                }
-            else
-                if(codigo==101)
-                    {
-                     valor=(quant*4.50);
-                    }
-                else
-                    if(codigo==102)
-                        {
-                        valor=(quant*5,20);
-                        }
-                    else
-                        if(codigo==103)
-                            {
-                            valor=(quant*3);
-                            }
-                        else
-                            if(codigo==104)
-                                {
-                                valor=(quant*4);
-                                }
-                            else
-                                {
-                                valor=(quant*2.50);
-                                }
-                    printf("O valor a ser pago e de %f reais",valor);
-*/
-  /*SEXTO EXERCICIO
-   //declaraÁ„o de vari·veis
-      int N, a, b, c;
-  //inicio
-        printf("Escolha um numero de 1 a 3: ");
-        scanf("%d",&N);
-        printf("Escolha um n˙mero A: ");
-        scanf("%d",&a);
-        printf("Escolha um n˙mero B: ");
-        scanf("%d",&b);
-        printf("Escolha um n˙mero C: ");
-        scanf("%d",&c);
-            if(N==1)
-            {
-                if(a<b&&b<c)
-                {
-                    printf("Os valores em ordem crescente e: %d %d %d",a,b,c);
-                }else
-                    if(b<a&&a<c)
-                    {
-                        printf("Os valores em ordem crescente e: %d %d %d",b,a,c);
-                    }else
-                        if(c<b&&b<a)
-                        {
-                            printf("Os valores em ordem crescente e: %d %d %d",c,b,a);
-                        }else
-                            if(c<a&&a<b)
-                            {
-                                 printf("Os valores em ordem crescente e: %d %d %d",c,a,b);
-                            }else
-                                if(b<c&&c<a)
-                                {
-                                      printf("Os valores em ordem crescente e: %d %d %d",b,c,a);
-                                }else
-                                    {
-                                         printf("Os valores em ordem crescente e: %d %d %d",a,c,b);
-                                    }
-
-            }else
-                if(N==2)
-                {
-                   if(a>b&&b>c)
-                    {
-                        printf("Os valores em ordem crescente e: %d %d %d",a,b,c);
-                    }else
-                        if(b>c&&c>a)
-                        {
-                            printf("Os valores em ordem crescente e: %d %d %d",b,c,a);
-                        }else
-                            if(c>a&&a>b)
-                            {
-                                printf("Os valores em ordem crescente e: %d %d %d",c,a,b);
-                            }else
-                                if(b>a&&a>c)
-                                {
-                                    printf("Os valores em ordem crescente e: %d %d %d",b,a,c);
-                                }else
-                                    if(c>b&&b>a)
-                                    {
-                                        printf("Os valores em ordem crescente e: %d %d %d",c,b,a);
-                                    }else
-                                        {
-                                           printf("Os valores em ordem crescente e: %d %d %d",a,c,b);
-                                        }
-                }else
-                    {
-                      if(a>b&&b>c)
-                        {
-                        printf("%d %d %d",b,a,c);
-                        }else
-                        if(b>c&&c>a)
-                        {
-                            printf("%d %d %d",c,b,a);
-                        }else
-                             if(c>a&&a>b)
-                            {
-                                printf("%d %d %d",a,c,b);
-                            }else
-                                if(b>a&&a>c)
-                                {
-                                    printf("Os valores em ordem crescente e: %d %d %d",a,b,c);
-                                }else
-                                    if(c>b&&b>a)
-                                    {
-                                        printf("Os valores em ordem crescente e: %d %d %d",b,c,a);
-                                    }else
-                                        {
-                                           printf("Os valores em ordem crescente e: %d %d %d",c,a,b);
-                                            }
-
-                        }
-
-*/
-
-}
+  }
 
 
 
